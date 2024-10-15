cc3c446ff494a03b5daff6e610740868b1976793
Aymeric GRANIER <shopidev34@gmail.com>
2024-10-15 21:21:01 +0200

    config local iphone 2

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,14 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Permet d'écouter sur toutes les interfaces réseau
+    host: '0.0.0.0',
     port: 5173,
-    https: {
+    https: process.env.NODE_ENV === 'development' ? {
       key: fs.readFileSync(path.resolve(__dirname, 'certs/privatekey.pem')),
       cert: fs.readFileSync(path.resolve(__dirname, 'certs/certificate.pem')),
-    }
-    // Le port utilisé pour le serveur de développement
+    } : false // Pas de HTTPS en production
   }
 })
